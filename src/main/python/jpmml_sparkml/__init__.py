2f3f21c234d05a2a21d1527902bb8a5d5cece560
Villu Ruusmann <villu.ruusmann@gmail.com>
2016-10-02 18:09:40 +0300

    Added Py4J error diagnostics

--- src/main/python/jpmml_sparkml/__init__.py
+++ src/main/python/jpmml_sparkml/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from py4j.java_gateway import JavaClass
 from pyspark.ml.common import _py2java
 
 __copyright__ = "Copyright (c) 2016 Villu Ruusmann"
@@ -9,5 +10,10 @@ __version__ = "${project.python_version}"
 def toPMMLBytes(sc, df, pipelineModel):
 	javaDF = _py2java(sc, df)
 	javaSchema = javaDF.schema.__call__()
+	
 	javaPipelineModel = pipelineModel._to_java()
-	return sc._jvm.org.jpmml.sparkml.ConverterUtil.toPMMLByteArray(javaSchema, javaPipelineModel)
+	
+	javaConverter = sc._jvm.org.jpmml.sparkml.ConverterUtil
+	if(not isinstance(javaConverter, JavaClass)):
+		raise RuntimeError("JPMML-SparkML not found on classpath")
+	return javaConverter.toPMMLByteArray(javaSchema, javaPipelineModel)
