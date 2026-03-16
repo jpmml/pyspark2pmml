a9bf87e11e7b9f7d81b4561739f563a8353a3bfa
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-03-16 22:16:14 +0200

    Removed redundant '_py2java' method calls

--- pyspark2pmml/__init__.py
+++ pyspark2pmml/__init__.py
@@ -1,13 +1,12 @@
 from py4j.java_gateway import JavaClass
-from pyspark.ml.common import _py2java
 
 from .metadata import __copyright__, __license__, __version__
 
 class PMMLBuilder(object):
 
 	def __init__(self, sc, df, pipelineModel):
-		javaDf = _py2java(sc, df)
-		javaSchema = javaDf.schema.__call__()
+		javaDf = df._jdf
+		javaSchema = javaDf.schema()
 		javaPipelineModel = pipelineModel._to_java()
 		javaPmmlBuilderClass = sc._jvm.org.jpmml.sparkml.PMMLBuilder
 		if(not isinstance(javaPmmlBuilderClass, JavaClass)):
@@ -31,29 +30,23 @@ class PMMLBuilder(object):
 		return javaFile.getAbsolutePath()
 
 	def putOption(self, pipelineStage, key, value):
-		javaKey = _py2java(self.sc, key)
-		javaValue = _py2java(self.sc, value)
 		if pipelineStage is None:
-			self.javaPmmlBuilder.putOption(javaKey, javaValue)
+			self.javaPmmlBuilder.putOption(key, value)
 		else:
 			javaPipelineStage = pipelineStage._to_java()
-			self.javaPmmlBuilder.putOption(javaPipelineStage, javaKey, javaValue)
+			self.javaPmmlBuilder.putOption(javaPipelineStage, key, value)
 		return self
 
 	def putFieldName(self, column, name):
-		javaColumn = _py2java(self.sc, column)
-		javaName = _py2java(self.sc, name)
-		self.javaPmmlBuilder.putFieldName(javaColumn, javaName)
+		self.javaPmmlBuilder.putFieldName(column, name)
 		return self
 
 	def putFieldNames(self, column, names):
-		javaColumn = _py2java(self.sc, column)
-		javaNames = _py2java(self.sc, names)
-		self.javaPmmlBuilder.putFieldNames(javaColumn, javaNames)
+		self.javaPmmlBuilder.putFieldNames(column, names)
 		return self
 
 	def verify(self, df, precision = 1e-14, zeroThreshold = 1e-14):
-		javaDf = _py2java(self.sc, df)
+		javaDf = df._jdf
 		self.javaPmmlBuilder.verify(javaDf, precision, zeroThreshold)
 		return self
 
